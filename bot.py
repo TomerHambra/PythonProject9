9acbd50a765152dbc6a488323ff7b6dcfdfe812b
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2025-04-01 20:40:34 +0300

    updated problems

---
+++ bot.py
@@ -0,0 +1,11 @@
+import requests
+import time
+
+website_link = 'https://cpuoh2025.streamlit.app'
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(10)
+        client = requests.session()
+        req = client.get(website_link)
+        print(req.text)
