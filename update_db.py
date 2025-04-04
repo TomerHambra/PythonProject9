4b8d8cf57a9f461fb3dc45a642b0f1c537c706dd
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2025-04-04 14:51:25 +0300

    added etgar num

--- update_db.py
+++ update_db.py
@@ -9,6 +9,11 @@ dist_file_path = Path(__file__).parent / 'hashed_pw.pkl'
 with open(src_file_path) as f:
     di = json.load(f)
 
+users = di['usernames']
+print(users)
+for name, user in users.items():
+    user['etgar'] = 18
+
 with open(dist_file_path, 'wb') as f:
     pickle.dump(di, f)
 
