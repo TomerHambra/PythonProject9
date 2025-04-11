e93216031cb2e76a53c0c04cfa949b1603eaf429
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2025-04-11 18:55:38 +0300

    added admin differential

--- leaderboard.py
+++ leaderboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from pathlib import Path
 import pickle
+import pandas as pd
+
 import data
 
 admins = data.admins
@@ -14,7 +16,7 @@ def update_scores(di, amtofq = len(data.stars)):
         l2 = [user, users[user]['etgar']]
         for i in range(amtofq):
             su += (stars[i] + 1) * (stars[i] + 1) if users[user].get(f"{i}", 2) == 0 else 0
-            l2.append('✅' if users[user].get(f"{i}", 2) == 0 else '❌' if users[user].get(f"{i}", 2) == 1 else '\-')
+            l2.append('✅' if users[user].get(f"{i}", 2) == 0 else '❌' if users[user].get(f"{i}", 2) == 1 else '\\-')
         temp = l2[1]
         l2[1] = su
         l2.append(temp)
@@ -37,10 +39,21 @@ for i, user in enumerate(sorted_list):
         sorted_list[i][0] = f":{coding[i]}[{user[0]}]"
     sorted_list[i][1] = f"**{sorted_list[i][1]}**"
 categories = ['Username', 'Total Score'] + [f'Problem {i+1}' for i in range(len(stars))] + ['Etgar']
-df = {}
-for i, stri in enumerate(categories):
-    df[stri] = [x[i] for x in sorted_list]
-print(df)
+# df = []
+# for i, stri in enumerate(categories):
+#     df.append([x[i] for x in sorted_list])
+# print(df)
+df = pd.DataFrame.from_records(sorted_list, columns=categories)
+et18 = st.checkbox('Show etgar 18', value=True)
+if not et18:
+    df.drop(df[df['Etgar'] == '18'].index, inplace=True)
+et19 = st.checkbox('Show etgar 19', value=True)
+if not et19:
+    df.drop(df[df['Etgar'] == '19'].index, inplace=True)
+# et20 = st.checkbox('Show etgar 20', value=True)
+# if not et19:
+#     df.drop(df[df['Etgar'] == '20'].index, inplace=True)
+
 st.table(df)
 
 
