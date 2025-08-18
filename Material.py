332b20dc805b6b91a4088422721fccf748ae049d
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2025-08-18 23:27:39 +0300

    material rework

--- Material.py
+++ Material.py
@@ -11,7 +11,7 @@ admins = data.admins
 st.title("Our Notebook (Partially)")
 st.header("Here you will find the algorithm implementations for things we believe you need.")
 
-t = st.tabs(["Dinic", "SCC", "Union Find", "MCMF", "Hungarian", "Seg Tree", "Lazy Seg Tree"])
+t = st.tabs(["Dinic", "SCC", "Union Find", "MCMF", "Hungarian", "Seg Tree", "Lazy Seg Tree", "Fenwick"])
 
 with t[0]:
     st.markdown("""
@@ -369,3 +369,44 @@ with t[6]:
 
     """
     st.code(code, language='cpp', line_numbers=True)
+
+with t[7]:
+    st.markdown('''
+    Description: Query $[0, i)$ and $[l, r)$ sums, and point updates.
+    
+    kth() returns the smallest index i s.t. query($i$) $> k$.
+    
+    Right endpoint is exclusive.
+    
+    Time: $O(\\log n)$ for all ops.''')
+
+    code = """
+
+    template<class T> struct Fenwick {
+        vector<T> s;
+        int n;
+        Fenwick(int n): s(n + 1), n(n) {}
+        void update(int i, T v) {
+            for (i++; i <= n; i += i & -i) s[i] += v; 
+        }
+        T query(int i) { // EXCLUSIVE interval [0, i)
+            T ans = 0;
+            for (; i; i -= i & -i) ans += s[i];
+            return ans; 
+        } // INCLUSIVE-EXCLUSIVE interval [l, r)
+           
+        T query(int l, int r) { return query(r) - query(l); }
+        
+        int kth(T k) { // 0-indexed, returns n if k > sum of tree
+            if (k < 0) return -1;
+            int i = 0;
+            for (int pw = 1 << __lg(n); pw; pw >>= 1)
+                if (i + pw <= n && s[i + pw] <= k) 
+                    k -= s[i += pw];
+            return i; 
+        } 
+    };
+    """
+    st.code(code, language='cpp', line_numbers=True)
+
+
